5d4e1e9d94750dadac4b730780dd8abc359c90d5
V01D <58613685+V01D-NULL@users.noreply.github.com>
2020-09-21 17:48:29 +0200

    Add files via upload

--- drivers/keyb.asm
+++ drivers/keyb.asm
@@ -1,32 +1,62 @@
+;;;;;;;;;;;;;;;;;;;;;
+; Game input (WASD, Arrow keys)
+;
+    KEY_W equ 0x77
+    KEY_TEXT_MODE equ 0x73   ; "s"
+    KEY_VIDEO_MODE equ 0x76  ; "v"
+    KEY_HELP equ 0x68        ; "h"
+;
+;;;;;;;;;;;;;;;;;;;;;
+
 shell:
-    pusha
-    mov si, shell_init_msg
-    call print
-    popa
+;     mov si, shell_init_msg
+;     call print
     mov si, shell_input_msg
     call print
-    jmp handle_stdin
-    
-handle_stdin:
-    call commandLine
-    mov si, newline
-    call print
-    jmp handle_stdin
-    
+    jmp commandLine
+  
 commandLine:
-    xor cl, cl
     mov ax, 0x00
     int 0x16
-    cmp cl, 0x3F
-    je commandLine
     
+    ; Check keyboard input
+    cmp al, 0x0D ; Enter
+    je nl
+    
+    cmp al, KEY_TEXT_MODE 
+    je switch_mode_text
+    
+    cmp al, KEY_VIDEO_MODE
+    je switch_mode_video
+    
+    cmp al, KEY_HELP
+    je help_menu
+    
+    ; Print input
     mov ah, 0x0E
     int 0x10
-;     int 0x19 ; Reboot
-    ; if (al == "enterkey") -> ret
+        
+nl:
+    mov si, newline
+    call print
+    mov si, shell_input_msg
+    call print
     jmp commandLine
-;     ret
+
+switch_mode_text:  
+    call mode_text
+    jmp shell
     
-shell_init_msg db "Loaded into sOS shell", 0x0A, 0x0D, 0
-shell_input_msg db "sOS> ", 0
-newline db "DONE", 0x0A, 0x0D, 0
+switch_mode_video:
+    call mode_video
+    jmp shell
+
+help_menu:
+    mov si, help_menu_text
+    call print
+    jmp shell
+    
+; shell_init_msg db "Loaded into skyEM shell", 0x0A, 0x0D, 0
+shell_input_msg db "skyEM{-}> ", 0
+newline db 0x0A, 0x0D, 0
+help_menu_text db 0x0A, 0x0D, "Help menu", 0x0A, 0x0D, "To toggle between screen resolutions, press s (text) or v (video).", 0x0A, 0x0D, "To list available games, type g", 0x0A, 0x0D, "To choose a game, type it's corresponding number.", 0x0A, 0x0D, "To play the selected game, press the spacebar", 0x0A, 0x0D, "To view the help menu, press h", 0x0A, 0x0D, 0
--- main.asm
+++ main.asm
@@ -1,12 +1,8 @@
 [org 0x7c00]
 
-;;;;;;;;;;;;;;;;;;;;;
-; Game input (WASD, Arrow keys)
+;   TODO:
+;       Load all the sectors from the drive that are available to skyEM
 ;
-    KEY_W equ 0x77
-    
-;
-;;;;;;;;;;;;;;;;;;;;;
 
 ; FIND OUT HOW TO  PRINT COLORFUL CHARACTERS AND MAKE A GAME SELECTION SCREEN OUT OF IT OR SOMETHING
 
@@ -15,58 +11,22 @@ call mode_text
 mov si, version
 call print
 
-mov si, msg
-call print
+; mov si, msg
+; call print
 
-call cli
-; call shell
+call shell
 
 jmp $
 
-cli:
-    mov ax, 0x00
-    int 0x16
-    cmp al, 0x0D
-    je nl
-    cmp al, KEY_W
-    je w_key
-    cmp al, txt_mode
-    je new_mode_text
-    cmp al, vid_mode
-    je vid_mode
-    mov ah, 0x0E
-    int 0x10
-    jmp cli
-
-w_key:
-    mov si, version
-    call print
-    jmp cli
-    
-nl:
-    mov si, newline
-    call print
-    jmp cli
-
-new_mode_text:  
-    call mode_text
-    ret
-    
-new_mode_video:
-    call mode_video
-    ret
     
 %include "stdio/print.asm"
 %include "vga/mode.asm"
-; %include "drivers/keyb.asm"
+%include "drivers/keyb.asm"
 %include "vga/gfx_256.asm"
 
 version db "skyEM - The Skyline retro game Emulator - 16 bits", 0x0A, 0x0D, 0
-newline db 0x0A, 0x0D, 0 ; Put this in keyb.asm
-exit db "exit", 0x0D
-txt_mode db "s", 0
-vid_mode db "v", 0
-msg db 0xDF, 0 ; Square block - To render extended ascii chars just put in the hex value of the ascii char
+
+; msg db 0xDF, 0 ; Square block - To render extended ascii chars just put in the hex value of the ascii char
 
 times 510-($-$$) db 0
 dw 0xaa55
